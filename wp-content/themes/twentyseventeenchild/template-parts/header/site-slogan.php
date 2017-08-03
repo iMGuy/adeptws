23d82c5ab51d2ea8aa350f0c330de32209b17ff7
Guy <guy.ads@gmail.com>
2017-08-03 19:20:14 +0000

    FrontPage slogan design issues fix (mobile display)

--- wp-content/themes/twentyseventeenchild/template-parts/header/site-slogan.php
+++ wp-content/themes/twentyseventeenchild/template-parts/header/site-slogan.php
@@ -19,10 +19,12 @@
 <div id="call-to-action" class="container-fluid">
 	<h1 class="marquee-brand">ניהול פרויקטים</h1>
 	<h2 class="marquee-heading">פיתוח אתרים, חנויות, מערכות מידע ואפליקציות</h2>
-	<p class="nicetomeet">
-		מעל עשור של ניסיון בפיתוח וניהול מוצרים אינטרנטיים.<br />
+	<span class="nicetomeet">
+		מעל עשור של ניסיון בפיתוח וניהול מוצרים אינטרנטיים.
+	</span><Br />
+	<span class="nicetomeet show_head">
 		ניהול פרויקטים, תכנון ותיעוד, פיתוח Front&Back, בדיקות, פריסה, ניהול גרסאות ועוד.
-	</p>
+	</span>
 	<ul class="list-inline">
 		<li><a href="#panel4" class="btn">איך מתקדמים? לחצ/י כאן!</a></li>
 	</ul>
